1d861e07941835efdb36ca20c6f43744c96db3c2
baguchan <shutux500@gmail.com>
2025-01-01 07:40:05 +0900

    Initial commit

---
+++ common/src/main/java/baguchi/maid_with_armor/MaidWithArmor.java
@@ -0,0 +1,9 @@
+package baguchi.maid_with_armor;
+
+
+public class MaidWithArmor {
+    public static final String MOD_ID = "maid_with_armor";
+
+    public static void init() {
+    }
+}
---
+++ fabric/src/main/java/baguchi/maid_with_armor/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package baguchi.maid_with_armor.fabric;
+
+import net.examplemod.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
